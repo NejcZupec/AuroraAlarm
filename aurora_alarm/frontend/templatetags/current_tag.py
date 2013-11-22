77e19f1cbc53cc41036d2c27214c5b80ba3746c6
Nejc Zupec <zupec.nejc@gmail.com>
2013-11-22 21:04:10 +0100

    Added current active link to the top navigation.

--- aurora_alarm/frontend/templatetags/current_tag.py
+++ aurora_alarm/frontend/templatetags/current_tag.py
@@ -1,10 +1,27 @@
 from django import template
+from django.core.urlresolvers import reverse
+
 
 register = template.Library()
 
 @register.simple_tag
-def current(request, pattern):
-    import re
-    if re.search(pattern, request.path):
+def current(request, name, by_path=False):
+    """ Return the string 'current' current request.path is same as name
+
+    This tag is used for top menu in the header.
+
+    Keyword aruguments:
+    request  -- Django request object
+    name     -- name of the url or the actual path
+    by_path  -- True if name contains a url instead of url name
+    """
+
+    if by_path:
+        path = name
+    else:
+        path = reverse(name)
+
+    if request.path == path:
         return 'current'
+
     return ''
