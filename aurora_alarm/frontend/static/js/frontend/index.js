2e995e52b5453f2d9d2c94faa3c2d7d97ce30f72
Nejc Zupec <zupec.nejc@gmail.com>
2013-12-05 17:24:32 +0100

    Removed urllib2 requests from controller. Now all calls are made with AJAX, close #23.

--- aurora_alarm/frontend/static/js/frontend/index.js
+++ aurora_alarm/frontend/static/js/frontend/index.js
@@ -1,3 +1,30 @@
 $( document ).ready(function() {
 
+    // prepare today's date in Python's format
+    var d = new Date();
+    var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+
+    // get aurora daily value via API, if exists in database
+    $.ajax({
+        type: "GET",
+        url: API_URL + "aurora_daily_forecast/?date=" + date,
+        success: function(data) {
+
+            // if aurora value for today exists, show current value and the progress bar
+            if (data.count != 0) {
+                var current_value = data.results[0].current_value;
+
+                $("#aurora-value-progress-bar").show();
+                $("#current-aurora-value").html(current_value);
+
+                /* Calculate a value for progressbar (convert between two intervals)
+                        Input interval: 0...5
+                        Output interval: 0...100 */
+                var interval_value = 100.0/5 * current_value;
+
+                $("#aurora-value-progress-bar .progress-bar").css("width", interval_value + "%");
+            }
+        }
+    });
+
 });
--- aurora_alarm/frontend/views.py
+++ aurora_alarm/frontend/views.py
@@ -1,7 +1,6 @@
-from django.conf import settings
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views import generic
 
 
